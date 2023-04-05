cc5894dc0ecbfe1707e48861d40e68758afc23c8
Shibbir Ahmad Shihab <127712036+shibbirahmadshihabweb@users.noreply.github.com>
2023-04-05 23:50:59 +0600

    Add files via upload

---
+++ js 1/jS/index.js
@@ -0,0 +1,41 @@
+
+var num3 = 25;
+
+console.log(typeof(num3));
+
+
+
+
+
+var num4 = "25";
+
+console.log(typeof(num4));
+
+
+
+
+
+var num = 20;
+
+num = toString(num);
+
+console.log(typeof(num));
+
+
+
+
+
+ var face = "20";
+
+face = parseInt(face);
+
+ console.log(typeof(face));
+
+
+
+
+ var num2 = "20.5";
+
+num2 = parseFloat(num2);
+
+console.log(typeof(num2));
