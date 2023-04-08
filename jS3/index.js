2c9e5a91966443d95ab30d467deb1a4a668a0ba9
Shibbir Ahmad Shihab <127712036+shibbirahmadshihabweb@users.noreply.github.com>
2023-04-08 23:48:20 +0600

    Add files via upload

---
+++ jS3/index.js
@@ -0,0 +1,56 @@
+/*
+  var text = "Bangladesh";
+
+  var len = text.length;
+
+  document.write('Number of caracter ' + len);
+
+  document.write(text.length);
+
+
+
+var v1 = prompt("Enter your name : ");
+
+document.write("Number of caracter " + v1.length);
+
+
+var v2 = "Bangladesh";
+document.write(v2.charAt(5))
+*/
+
+/*
+var v3 = "Bangladesh ";
+
+v3 = v3.toUpperCase();
+document.write(v3);
+
+v3 = v3.toLowerCase();
+document.write(v3);
+
+document.write(v3.toUpperCase());
+document.write(v3.toLowerCase());
+*/
+/*
+var text1 = "Bangladesh";
+var text2 = " is a beautiful country. "
+
+var text = text1.concat(text2);
+
+document.write(text);
+document.write(text1.concat(text2));
+*/
+/*
+var text3 = "Bangladesh";
+var text = text3.slice(0,5);
+document.write(text);
+document.write(text3.slice(5,10));
+*/
+
+var text1 = prompt("First name: ")
+var text2 = prompt("last name: ")
+// var text = text1.concat(text2);
+var text = text1 + text2;
+document.write(text );
+document.write("your full name caracter is : " + text.length );
+document.write(text.toUpperCase());
+document.write(text.charAt(3));
