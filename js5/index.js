7a2251bedcd61608bba53f15d4a56cc727718619
Shibbir Ahmad Shihab <127712036+shibbirahmadshihabweb@users.noreply.github.com>
2023-04-11 01:05:45 +0600

    Add files via upload

---
+++ js5/index.js
@@ -0,0 +1,8 @@
+var base = parseFloat(prompt("Enter base : "));
+var height = parseFloat(prompt("Enter height : "));
+
+var area = base * height;
+
+
+document.write("Rectangle Area = " + area + "<br/>");
+document.write("Triangle Area = " + (area) /2);
